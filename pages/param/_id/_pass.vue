29ac60f73a2f8439eb423e7066f3e2550fc8a8d9
JunHatanaka <nextseason6@gmail.com>
2024-03-24 03:15:24 +0900

    feat: param制御ページ

---
+++ pages/param/_id/_pass.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="container">
+    <h1>{{ title }}</h1>
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <p v-html="message"></p>
+    <hr />
+    <div class="right">
+      <router-link to="/">Go to Top</router-link>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    message() {
+      const id =
+        this.$route.params.id !== undefined
+          ? this.$route.params.id
+          : '*** no id ***'
+      const pass =
+        this.$route.params.pass !== undefined
+          ? this.$route.params.pass
+          : '*** no password ***'
+      return 'ID：' + id + '<br>PASS：' + pass
+    },
+  },
+}
+</script>
+
+<style>
+.container {
+  padding: 5px 10px;
+}
+
+h1 {
+  font-size: 60pt;
+  color: #345980;
+}
+
+p {
+  padding-top: 5px;
+  font-size: 20pt;
+}
+
+a {
+  font-size: 16pt;
+}
+
+.right {
+  text-align: right;
+}
+</style>
