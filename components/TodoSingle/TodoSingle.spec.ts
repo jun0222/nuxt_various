40150ec725278ffad374a341a88f9593cdf16e32
JunHatanaka <nextseason6@gmail.com>
2024-03-31 01:34:03 +0900

    fix: テストを修正

--- components/TodoSingle/TodoSingle.spec.ts
+++ components/TodoSingle/TodoSingle.spec.ts
@@ -1,6 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
 import MockAdapter from 'axios-mock-adapter'
 import axios from 'axios'
+import flushPromises from 'flush-promises'
 import TodoSingle from './TodoSingle.vue'
 
 const mock = new MockAdapter(axios)
@@ -8,20 +9,31 @@ const mock = new MockAdapter(axios)
 describe('TodoSingle.vue', () => {
   beforeEach(() => {
     mock.reset()
+
+    // POSTリクエスト（新しいTODOの追加）に対するモックの設定
+    mock.onPost('http://localhost:3001/path/to/todos').reply(201, {
+      id: 2, // 新しく追加される想定のTODOのID
+      title: 'New Test Todo', // 新しく追加される想定のTODOのタイトル
+    })
+
+    // GETリクエスト（TODOリストの取得）に対するモックの設定
+    // ここでは、新しく追加されたTODOが既にリストに含まれている状態を模倣
+    mock.onGet('http://localhost:3001/path/to/todos').reply(200, [
+      { id: 1, title: 'Test Todo' }, // 既存のTODO
+      { id: 2, title: 'New Test Todo' }, // 新しく追加されたTODO
+    ])
   })
 
   it('fetches todos on mount and renders them', async () => {
-    mock
-      .onGet('http://localhost:3001/path/to/todos')
-      .reply(200, [{ id: 1, title: 'Test Todo' }])
-
     const wrapper = shallowMount(TodoSingle, {
       mocks: {
         axios,
       },
     })
 
-    await wrapper.vm.$nextTick()
-    expect(wrapper.findAll('li').length).toBe(1)
+    await flushPromises() // 全てのプロミスが解決されるのを待つ
+    await wrapper.vm.$nextTick() // VueのDOM更新が完了するのを待つ
+
+    expect(wrapper.findAll('li').length).toBe(2) // 追加されたTodoも含めた総数をチェック
   })
 })
